f4464290017e13e48dd1addf82a0502db6adf64e
David <dducrest@gmail.com>
2014-03-07 18:51:02 -0500

    created basic ruby project

---
+++ agj_ballot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AgjBallot::Application.config.secret_key_base = '7a225d117367f8be78a0ecb8243d8e87a1908c1213fb4d63b4970da8b0d3321128c64aadd06a32f964c0b8592d6ee69dbc9d00d15b2e3ab4c6b879308e05b53e'
---
+++ agj_ballot/db/migrate/20140307234222_create_participants.rb
@@ -0,0 +1,11 @@
+class CreateParticipants < ActiveRecord::Migration
+  def change
+    create_table :participants do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
